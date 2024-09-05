cb4bbbcd5afac143d3798c03320ad2949a80cace
patrick <patrick@ms.touk.io>
2024-09-05 16:22:59 -0300

    refact

--- models/token.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type AuthToken struct {
-	ID        string `bson:"_id,omitempty"`
-	Token     string `bson:"token"`
-	UserID    string `bson:"user_id"`
-	ExpiresAt int64  `bson:"expires_at"`
-}
--- models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `bson:"username,omitempty"`
-	Password string             `bson:"password,omitempty"`
-}
